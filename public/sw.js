b21f2a591d255192c62d6abd8612743827db38b0
Tamkin Anwar <tamkin-anwar@users.noreply.github.com>
2026-09-15 20:17:12 -0700

    Match original Jotfield leaf logo exactly

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jotfield-app-9';
+const CACHE_NAME = 'jotfield-app-10';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './jotfield-icon.svg', './favicon.ico', './jotfield-icon-192.png', './jotfield-icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', (event) => {
